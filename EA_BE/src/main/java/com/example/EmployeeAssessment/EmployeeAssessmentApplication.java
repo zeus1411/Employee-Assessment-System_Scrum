63e8f71088ac49d8406ebafd671b8732f12bc1d4
hcmute-toan <vancongtoan09102004@gmail.com>
2025-06-17 21:56:27 +0700

    Create Base Project EA

---
+++ EA_BE/src/main/java/com/example/EmployeeAssessment/EmployeeAssessmentApplication.java
@@ -0,0 +1,15 @@
+package com.example.EmployeeAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(exclude =
+org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+public class EmployeeAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAssessmentApplication.class, args);
+	}
+
+}
---
+++ EA_BE/src/test/java/com/example/EmployeeAssessment/EmployeeAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
